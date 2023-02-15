9a41d448720c480f3361d77cbac2d2b3f8392869
neb0002 <neb0002@alu.medac.es>
2023-02-15 18:59:11 +0100

    hola

---
+++ media.py
@@ -0,0 +1,15 @@
+class Media:
+    notas=[]
+    def notas(self):
+        return self.notas
+    
+    def add(self, number):
+        if number < 0 or number >10:
+            raise ValueError
+        self.notas.append(number)
+    
+    def media(self):
+        suma = 0
+        for i in self.notas:
+            suma = suma + i
+        return suma/len(self.notas)
